bdac57fb091489544a2d6f6285e4f0f6031ba749
Claudie.Michalon <michalon.claudie@outlook.fr>
2024-06-02 18:07:55 +0200

    add slider

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,19 @@
-<script>
+<script lang="ts">
     import "../app.css";
+    import { AppBar } from '@skeletonlabs/skeleton';
+    import { AppShell } from '@skeletonlabs/skeleton';
+										
   </script>
-  <div class="bg-blue-400">
-  <slot />
-  </div>
   
+  <AppShell class="bg-primary-300">
+      <svelte:fragment slot="header">
+      <AppBar class="bg-gradient-to-b from-secondary-300 to-primary-300">
+        <svelte:fragment slot="lead">(icon)</svelte:fragment>
+        (title)
+        <svelte:fragment slot="trail">(actions)</svelte:fragment>
+      </AppBar>
+    </svelte:fragment>
+      <svelte:fragment slot="pageHeader">Page Header</svelte:fragment>
+      <slot />
+      <svelte:fragment slot="pageFooter">Page Footer</svelte:fragment>
+  </AppShell>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,46 @@
 <script lang="ts">
     export let hello:string='';
+    let elemCarousel: HTMLDivElement;
+const unsplashIds = [ '1', '2', '3', '4'];
+
+function carouselLeft(): void {
+	const x =
+		elemCarousel.scrollLeft === 0
+			? elemCarousel.clientWidth * elemCarousel.childElementCount // loop
+			: elemCarousel.scrollLeft - elemCarousel.clientWidth; // step left
+	elemCarousel.scroll(x, 0);
+}
+
+function carouselRight(): void {
+	const x =
+		elemCarousel.scrollLeft === elemCarousel.scrollWidth - elemCarousel.clientWidth
+			? 0 // loop
+			: elemCarousel.scrollLeft + elemCarousel.clientWidth; // step right
+	elemCarousel.scroll(x, 0);
+}
 </script>
 <h1 class="text-3xl">Welcome to SvelteKit</h1>
 <p>Visit {hello} <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<div class="card p-4 grid grid-cols-[auto_1fr_auto] gap-4 items-center">
+	<!-- Button: Left -->
+	<button type="button" class="btn-icon variant-filled" on:click={carouselLeft}>
+		<i class="fa-solid fa-arrow-left" />
+	</button>
+	<!-- Full Images -->
+	<div bind:this={elemCarousel} class="snap-x snap-mandatory scroll-smooth flex overflow-x-auto">
+		{#each unsplashIds as unsplashId}
+			<img
+				class="snap-center w-[1024px] rounded-container-token"
+				src="https://source.unsplash.com/{unsplashId}/1024x768"
+				alt={unsplashId}
+				loading="lazy"
+			/>
+		{/each}
+	</div>
+	<!-- Button: Right -->
+	<button type="button" class="btn-icon variant-filled" on:click={carouselRight}>
+		<i class="fa-solid fa-arrow-right" />
+	</button>
+</div>
+
