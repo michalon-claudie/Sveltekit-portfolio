90194061b275fc35a2ae00ae30f5c9f7b220d1e1
Claudie.Michalon <michalon.claudie@outlook.fr>
2024-06-02 17:54:03 +0200

    add tailwind package

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    export let hello:string='';
+</script>
+<h1 class="text-3xl">Welcome to SvelteKit</h1>
+<p>Visit {hello} <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
